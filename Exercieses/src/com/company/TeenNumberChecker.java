c7b6571e09439acacb9d12f56eecb91d24b7eea0
KevinW <bird831205@hotmail.com>
2019-09-28 01:24:23 -0400

    more practice

---
+++ Exercieses/src/com/company/TeenNumberChecker.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class TeenNumberChecker {
+    public static boolean hasTeen (int a, int b, int c){
+        if(isTeen(a) || isTeen(b) || isTeen(c)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isTeen (int a){
+        if(a>=13 && a <=19){
+            return true;
+        }
+        return false;
+    }
+}
