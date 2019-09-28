3da27ca18795e478d8128929652b0f2174a88e67
KevinW <bird831205@hotmail.com>
2019-09-27 21:04:04 -0400

    methods continue

--- codeBlock/src/com/company/Main.java
+++ codeBlock/src/com/company/Main.java
@@ -3,24 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         boolean gameOver = true;
         int score = 5000;
         int level = 2;
         int bonus = 100;
 
-        calculateScore(gameOver,score,level,bonus);
+        calculateScore(gameOver, score, level, bonus);
 
-        calculateScore(false,0,0,0);
+        calculateScore(false, 0, 0, 0);
 
-        calculateScore(true,1000,5,300);
+        calculateScore(true, 1000, 5, 300);
 
-        int finalScore = calculateScoreWReturn(gameOver,score,level,bonus);
+        int finalScore = calculateScoreWReturn(gameOver, score, level, bonus);
         System.out.println("calculate with return " + finalScore);
 
-        finalScore = calculateScoreWReturn (false,score,level,bonus);
+        finalScore = calculateScoreWReturn(false, score, level, bonus);
         System.out.println("calculate with return " + finalScore);
 
+        String displayedString = displayHighScorePosition("mark", 12);
+        System.out.println(displayedString);
+
+        int position = calculateHighScorePosition(300);
+        displayedString = displayHighScorePosition("unknown", position);
+        System.out.println(displayedString);
 
 //        if(gameOver){
 //            System.out.println("Game Over");
@@ -45,19 +51,38 @@ public class Main {
 //        System.out.println("second score "+finalScore );
     }
 
-    public static void calculateScore(boolean gameOver, int score, int level, int bonus){
-        if(gameOver){
-            int finalScore = score+level*bonus;
+    public static void calculateScore(boolean gameOver, int score, int level, int bonus) {
+        if (gameOver) {
+            int finalScore = score + level * bonus;
             System.out.println("score " + finalScore);
 
         }
     }
 
-    public static int calculateScoreWReturn(boolean gameOver, int score, int level, int bonus){
-        if(gameOver){
-            int finalScore = score+level*bonus;
+    public static int calculateScoreWReturn(boolean gameOver, int score, int level, int bonus) {
+        if (gameOver) {
+            int finalScore = score + level * bonus;
             return finalScore;
         }
         return -1;
+    }
+
+    public static String displayHighScorePosition(String playerName, int position) {
+
+        return playerName + " got into position " + position;
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+
+
     }
 }
