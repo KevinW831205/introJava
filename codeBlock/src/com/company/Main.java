56001fc6bfa2d8e795765e01df66009a0146006d
KevinW <bird831205@hotmail.com>
2019-09-27 20:02:47 -0400

    java methods

--- codeBlock/src/com/company/Main.java
+++ codeBlock/src/com/company/Main.java
@@ -9,28 +9,55 @@ public class Main {
         int level = 2;
         int bonus = 100;
 
+        calculateScore(gameOver,score,level,bonus);
+
+        calculateScore(false,0,0,0);
+
+        calculateScore(true,1000,5,300);
+
+        int finalScore = calculateScoreWReturn(gameOver,score,level,bonus);
+        System.out.println("calculate with return " + finalScore);
+
+        finalScore = calculateScoreWReturn (false,score,level,bonus);
+        System.out.println("calculate with return " + finalScore);
+
+
+//        if(gameOver){
+//            System.out.println("Game Over");
+//            int finalScore =  score + level*bonus;
+//            System.out.println("Your score " + finalScore);
+//        } else if(level == 1 && !gameOver){
+//            System.out.println("game start");
+//            System.out.println("level " + level);
+//        } else {
+//            System.out.println("level " + level);
+//            System.out.println("current bonus "+bonus);
+//        }
+//
+//
+//        //scope test
+////        System.out.println(finalScore);
+//
+//        score = 10000;
+//        level = 8;
+//        bonus = 200;
+//        int finalScore = score+level*bonus;
+//        System.out.println("second score "+finalScore );
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int level, int bonus){
         if(gameOver){
-            System.out.println("Game Over");
-            int finalScore =  score + level*bonus;
-            System.out.println("Your score " + finalScore);
-        } else if(level == 1 && !gameOver){
-            System.out.println("game start");
-            System.out.println("level " + level);
-        } else {
-            System.out.println("level " + level);
-            System.out.println("current bonus "+bonus);
+            int finalScore = score+level*bonus;
+            System.out.println("score " + finalScore);
+
         }
+    }
 
-
-        //scope test
-//        System.out.println(finalScore);
-
-        score = 10000;
-        level = 8;
-        bonus = 200;
-        int finalScore = score+level*bonus;
-        System.out.println("second score "+finalScore );
-
-
+    public static int calculateScoreWReturn(boolean gameOver, int score, int level, int bonus){
+        if(gameOver){
+            int finalScore = score+level*bonus;
+            return finalScore;
+        }
+        return -1;
     }
 }
