79ebd0515ec5c27efeb02c4bb25eb2b75ec8445d
KevinW <bird831205@hotmail.com>
2019-09-29 22:47:34 -0400

    practice

--- Loops/src/com/company/Main.java
+++ Loops/src/com/company/Main.java
@@ -14,8 +14,27 @@ public class Main {
 //        for(int i = 8; i>1; i--){
 //            System.out.println(String.format("%.2f",calculateInterest(10000.0, (double) i)));
 //        }
-
-        PrimeNumber.ThreePrimeInRange(3,21);
+//
+//        PrimeNumber.ThreePrimeInRange(3,21);
+//        int sum =0;
+//        int count = 0;
+//        for(int i = 1; i <= 1000; i++){
+//
+//
+//            if(i%3 == 0 && i%5 == 0){
+//                System.out.println(i);
+//                sum += i;
+//                count++;
+//            }
+//
+//            if(count == 5){
+//                System.out.println("Sum is "+ sum);
+//                break;
+//            }
+//        }
+//
+        System.out.println(SumOddRange.sumOdd(-1,3));
+        System.out.println(SumOddRange.sumOdd(10,10));
 
 
     }
---
+++ Loops/src/com/company/SumOddRange.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class SumOddRange {
+
+    public static boolean isOdd(int num){
+        if(num>0 && num%2 == 1){
+            return true;
+        }
+        return false;
+    }
+
+    public static int sumOdd (int start, int end){
+        if(start>0 && end >0 && end>=start){
+            int sum =0;
+            for(int i = start; i<=end; i++){
+                if(isOdd(i)){
+                    sum += i;
+                }
+            }
+            return sum;
+        } else{
+            return -1;
+        }
+
+    }
+
+}
