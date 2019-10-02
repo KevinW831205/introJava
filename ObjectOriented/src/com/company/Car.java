67145d6ee68e916ff924d56963896dceaed6e1b4
KevinW <bird831205@hotmail.com>
2019-10-02 16:26:11 -0400

    java classes

--- ObjectOriented/src/com/company/Car.java
+++ ObjectOriented/src/com/company/Car.java
@@ -10,5 +10,21 @@ public class Car {
     private String color;
 //    public int fuel;  public give access to other classes.
 
+    public void setModel(String modelName){
+        String modelInput = modelName.toLowerCase();
+        if(modelInput.equals("compass") || modelInput.equals("cherokee")){
+            this.model = modelInput;
+        } else {
+            this.model="unknown";
+            System.out.println("not a valid model");
+        }
+
+
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
 
 }
--- ObjectOriented/src/com/company/Main.java
+++ ObjectOriented/src/com/company/Main.java
@@ -6,7 +6,13 @@ public class Main {
 	// write your code here
         Car jeep = new Car();
         Car toyota = new Car();
-
+        System.out.println();
         // jeep. to see methods and states related to class
+
+        jeep.setModel("compas");
+
+        jeep.setModel("compass");
+        String model1 = jeep.getModel();
+        System.out.println(model1);
     }
 }
