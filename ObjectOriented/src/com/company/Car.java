f16a9da5931ed9caba8b9e1db7e50a5c496af199
KevinW <bird831205@hotmail.com>
2019-10-02 09:04:09 -0400

    intro to classes

---
+++ ObjectOriented/src/com/company/Car.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Car {
+
+    // State component, fields
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+//    public int fuel;  public give access to other classes.
+
+
+}
---
+++ ObjectOriented/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Car jeep = new Car();
+        Car toyota = new Car();
+
+        // jeep. to see methods and states related to class
+    }
+}
