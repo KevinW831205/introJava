cf731e39b083f9d40444d64ab5a8a7d7c254de67
KevinW <bird831205@hotmail.com>
2019-10-02 20:33:13 -0400

    java constructors

---
+++ ObjectOriented/src/com/company/BankAccount.java
@@ -0,0 +1,111 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class BankAccount {
+    String accountNumber;
+    int balance;
+    String customerName;
+    String email;
+    String phoneNumber;
+
+    // constructor
+    public BankAccount(){
+        this("00000",0,"default","default","default");
+        System.out.println("constructor ran with default values");
+    }
+
+    public BankAccount(String accountNumber,int balance, String customerName, String email, String phoneNumber){
+        this.accountNumber=accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email =  email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        if(accountNumber.isEmpty()){
+            System.out.println("Cannot enter blank");
+        } else {
+            this.accountNumber = accountNumber;
+        }
+    }
+
+    public void setBalance(int balance) {
+        if(balance<0){
+            System.out.println("Cannot have negative balance");
+        } else{
+            this.balance = balance;
+        }
+    }
+
+    public void setCustomerName(String customerName) {
+        if(customerName.isEmpty()){
+            System.out.println("Cannot enter blank");
+        } else {
+            this.customerName = customerName;
+        }
+    }
+
+    public void setEmail(String email) {
+        if(email.isEmpty()){
+            System.out.println("Cannot enter blank");
+        } else {
+            this.email = email;
+        }
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if(phoneNumber.isEmpty()){
+            System.out.println("Cannot enter blank");
+        } else {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    public void depost(int deposit){
+        if(deposit<=0){
+            System.out.println("must deposit a positive number");
+        } else {
+            this.balance += deposit;
+            System.out.println("deposited "+deposit+ ", remaining "+ this.balance);
+
+        }
+    }
+
+    public void withdraw(int withdraw){
+        if(withdraw <=0){
+            System.out.println("must withdraw a positive number");
+            return;
+        }
+
+        if(this.balance-withdraw <0){
+            System.out.println("Insufficient funds");
+        } else{
+            this.balance-=withdraw;
+            System.out.println("withdrew "+withdraw+", remaining "+this.balance);
+        }
+
+
+    }
+}
--- ObjectOriented/src/com/company/Main.java
+++ ObjectOriented/src/com/company/Main.java
@@ -4,15 +4,57 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Car jeep = new Car();
-        Car toyota = new Car();
-        System.out.println();
-        // jeep. to see methods and states related to class
+//        Car jeep = new Car();
+//        Car toyota = new Car();
+//        System.out.println();
+//        // jeep. to see methods and states related to class
+//
+//        jeep.setModel("compas");
+//
+//        jeep.setModel("compass");
+//        String model1 = jeep.getModel();
+//        System.out.println(model1);
 
-        jeep.setModel("compas");
+        BankAccount myAccount = new BankAccount();
 
-        jeep.setModel("compass");
-        String model1 = jeep.getModel();
-        System.out.println(model1);
+        BankAccount myAccount2 = new BankAccount("123456",300,"kevin2","234@123.com","123-123-1235");
+
+        BankAccount defaultAccount = new BankAccount();
+
+
+        myAccount.setAccountNumber("12345");
+        myAccount.setBalance(500);
+        myAccount.setCustomerName("Kevin");
+        myAccount.setEmail("123@123.com");
+        myAccount.setPhoneNumber("123-123-1234");
+
+        System.out.println("account number: "+myAccount.getAccountNumber());
+        System.out.println("balance: "+myAccount.getBalance());
+        System.out.println("holder name: "+myAccount.getCustomerName());
+        System.out.println("email: "+myAccount.getEmail());
+        System.out.println("phone: "+myAccount.getPhoneNumber());
+
+        myAccount.depost(-400);
+        myAccount.depost(400);
+        myAccount.withdraw(300);
+
+        myAccount.withdraw(-300);
+        myAccount.withdraw(3000);
+
+
+        System.out.println("account number: "+myAccount2.getAccountNumber());
+        System.out.println("balance: "+myAccount2.getBalance());
+        System.out.println("holder name: "+myAccount2.getCustomerName());
+        System.out.println("email: "+myAccount2.getEmail());
+        System.out.println("phone: "+myAccount2.getPhoneNumber());
+
+        System.out.println("account number: "+defaultAccount.getAccountNumber());
+        System.out.println("balance: "+defaultAccount.getBalance());
+        System.out.println("holder name: "+defaultAccount.getCustomerName());
+        System.out.println("email: "+defaultAccount.getEmail());
+        System.out.println("phone: "+defaultAccount.getPhoneNumber());
+
+
+
     }
 }
