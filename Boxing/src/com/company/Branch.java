a3cd567f0ce62ff53af1ade88706dabe3fb00349
KevinW <bird831205@hotmail.com>
2019-10-08 16:16:57 -0400

    check balance and edit balance

--- Boxing/src/com/company/Branch.java
+++ Boxing/src/com/company/Branch.java
@@ -1,5 +1,45 @@
 package com.company;
 
-public class Branch {
+import java.util.ArrayList;
 
+public class Branch {
+    private ArrayList<Customer> customers = new ArrayList<Customer>();
+
+    public void addCustomer(String customerName) {
+        if (findCustomer(customerName) >= 0) {
+            System.out.println("Error creating account, customer name exists");
+        } else {
+            customers.add(new Customer((customerName)));
+            System.out.println(customerName + "'s account was created");
+        }
+    }
+
+    private int findCustomer(Customer customerCheck) {
+        return customers.indexOf(customerCheck);
+    }
+
+    private int findCustomer(String customerName) {
+        for (int i = 0; i < customers.size(); i++) {
+            if (customers.get(i).getCustomerName().equals(customerName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int checkBalance(String customerName){
+        int index = findCustomer(customerName);
+        if(index>=0){
+            checkBalance(index);
+        } else {
+            System.out.println("can't find customer name");
+        }
+    }
+
+    private int checkBalance(int customerIndex){
+        Customer customer = customers.get(customerIndex);
+        System.out.println(customer.getCustomerName() + "'s Account Transactions");
+        customer.getTransactions();
+        System.out.printf("Balance: "+ customer.getBalance());
+    }
 }
--- Boxing/src/com/company/Customer.java
+++ Boxing/src/com/company/Customer.java
@@ -18,4 +18,39 @@ public class Customer {
     public ArrayList<Integer> getTransactions() {
         return transactions;
     }
+
+    public void deposit(int amount){
+        if(amount>0){
+            transactions.add(amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(int amount){
+        if(amount>0){
+            if(getBalance() - amount >= 0){
+                transactions.add(amount * -1);
+                System.out.println("Withdrew "+amount);
+            } else {
+                System.out.println("Insufficient Balance");
+            }
+        } else {
+            System.out.println("Invalid withdraw amount");
+        }
+    }
+
+    public int getBalance(){
+        int sum =0;
+        for(int i=0; i<transactions.size();i++){
+            sum += transactions.get(i).intValue();
+        };
+        return sum;
+    }
+
+    public void printTransaction(){
+        for(int i=0; i<transactions.size();i++){
+            System.out.println(transactions.get(i).intValue());
+        };
+    }
 }
