2a9eea8043eb703d57cbca8da7240f9210d44169
KevinW <bird831205@hotmail.com>
2019-10-19 19:34:57 -0400

    location exits

--- Adventure/src/com/company/Main.java
+++ Adventure/src/com/company/Main.java
@@ -11,12 +11,33 @@ public class Main {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0,"location 1"));
-        locations.put(1, new Location(1,"location 2"));
-        locations.put(2, new Location(2,"location 3"));
-        locations.put(3, new Location(3,"location 4"));
-        locations.put(4, new Location(4,"location 5"));
-        locations.put(5, new Location(5,"location 6"));
+        locations.put(0, new Location(0,"location 0 (exit)"));
+        locations.put(1, new Location(1,"location 1"));
+        locations.put(2, new Location(2,"location 2"));
+        locations.put(3, new Location(3,"location 3"));
+        locations.put(4, new Location(4,"location 4"));
+        locations.put(5, new Location(5,"location 5"));
+
+        locations.get(1).addExit("W",2);
+        locations.get(1).addExit("E",3);
+        locations.get(1).addExit("S",4);
+        locations.get(1).addExit("N",5);
+//        locations.get(1).addExit("Q",0);
+
+        locations.get(2).addExit("N",5);
+//        locations.get(2).addExit("Q",0);
+
+        locations.get(3).addExit("W",1);
+//        locations.get(3).addExit("Q",0);
+
+        locations.get(4).addExit("N",1);
+        locations.get(4).addExit("W",2);
+//        locations.get(4).addExit("Q",0);
+
+        locations.get(5).addExit("S",1);
+        locations.get(5).addExit("W",2);
+//        locations.get(5).addExit("Q",0);
+
 
         int loc = 1;
         while (true){
@@ -25,10 +46,20 @@ public class Main {
                 break;
             }
 
-            loc = scanner.nextInt();
-            if(!locations.containsKey(loc)){
-                System.out.println("Cannot go to that location");
+            Map<String,Integer> exits = locations.get(loc).getExits();
+            System.out.println("Available exits are ");
+            for(String exit: exits.keySet()){
+                System.out.print(exit+", ");
             }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if(exits.containsKey(direction)){
+                loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go in that direction");
+            }
+
         }
 
 
