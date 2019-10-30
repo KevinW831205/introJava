7abbc69a9ee6da3560a32442f09739f0b312dfba
KevinW <bird831205@hotmail.com>
2019-10-30 16:53:13 -0400

    radio buttons

--- JavaFXControls/src/sample/Main.java
+++ JavaFXControls/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setScene(new Scene(root, 1800, 900));
         primaryStage.show();
     }
 
