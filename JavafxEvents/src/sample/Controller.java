bc75fc9f0eef0d41317903f0088b349aa4be7b9d
KevinW <bird831205@hotmail.com>
2019-10-31 20:25:46 -0400

    annotate event handler

--- JavafxEvents/src/sample/Controller.java
+++ JavafxEvents/src/sample/Controller.java
@@ -7,6 +7,7 @@ public class Controller {
     @FXML
     private TextField textField1;
 
+    @FXML
     public void onButtonClick(){
         System.out.println("Hello, you typed "+ textField1.getText());
     }
