5c3056d3b6f4acbdb7cab6c35b930f134bb15421
KevinW <bird831205@hotmail.com>
2019-10-31 20:23:49 -0400

    retrieve text from textfile

--- JavafxEvents/src/sample/Controller.java
+++ JavafxEvents/src/sample/Controller.java
@@ -1,8 +1,13 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField textField1;
 
     public void onButtonClick(){
-        System.out.println("Hello");
+        System.out.println("Hello, you typed "+ textField1.getText());
     }
 }
