3f65eea7cc7c0202261c744892df773d8011ac01
KevinW <bird831205@hotmail.com>
2019-11-01 20:32:38 -0400

    disabling buttons

--- JavafxEvents/src/sample/Controller.java
+++ JavafxEvents/src/sample/Controller.java
@@ -14,14 +14,32 @@ public class Controller {
     private Button byeButton;
 
     @FXML
+    public void initialize(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
     public void onButtonClick(ActionEvent e){
         if(e.getSource().equals(helloButton)){
             System.out.println("hello, your input "+textField1.getText());
-
+            textField1.clear();
         } else if(e.getSource().equals(byeButton)) {
             System.out.println("bye, your input "+textField1.getText());
+            textField1.clear();
         } else {
             System.out.println("unknown button");
+            textField1.clear();
         }
+    }
+
+
+
+    @FXML
+    public void handleKeyReleased(){
+        String text = textField1.getText();
+        boolean buttonsDisabled = text.isEmpty() | text.trim().isEmpty();
+        helloButton.setDisable(buttonsDisabled);
+        byeButton.setDisable(buttonsDisabled);
     }
 }
