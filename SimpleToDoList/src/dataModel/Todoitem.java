61671cf7ed27ae203c8f9a1d0deb3d90ee59f64d
KevinW <bird831205@hotmail.com>
2019-11-03 14:19:23 -0500

    setup todoitem wtih detail description and date

---
+++ SimpleToDoList/src/dataModel/Todoitem.java
@@ -0,0 +1,40 @@
+package dataModel;
+
+import java.time.LocalDate;
+
+public class Todoitem {
+
+    private String description;
+    private String details;
+    private LocalDate deadline;
+
+    public Todoitem(String description, String details, LocalDate deadline) {
+        this.description = description;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+}
