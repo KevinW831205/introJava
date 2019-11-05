df4a74edc01ca4193b29d3309974516df153b775
KevinW <bird831205@hotmail.com>
2019-11-05 18:28:16 -0500

    returning selected directory

--- CSSwJavaFX/src/sample/Controller.java
+++ CSSwJavaFX/src/sample/Controller.java
@@ -6,7 +6,10 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Controller {
     @FXML
@@ -16,27 +19,35 @@ public class Controller {
     @FXML
     private GridPane mainGridPane;
 
-    public void initialize(){
+    public void initialize() {
 //        label1.setScaleX(2.0);
 //        label1.setScaleY(2.0);
         button4.setEffect(new DropShadow());
     }
 
     @FXML
-    public void handleMouseOver(){
+    public void handleMouseOver() {
         label1.setScaleX(2.0);
         label1.setScaleY(2.0);
     }
 
     @FXML
-    public void handleMouseExit(){
+    public void handleMouseExit() {
         label1.setScaleX(1.0);
         label1.setScaleY(1.0);
     }
 
     @FXML
-    public void handleClick(){
-        FileChooser chooser = new FileChooser();
-        chooser.showOpenDialog(mainGridPane.getScene().getWindow());
+    public void handleClick() {
+//        FileChooser chooser = new FileChooser();
+//        chooser.showOpenDialog(mainGridPane.getScene().getWindow());
+        DirectoryChooser chooser = new DirectoryChooser();
+        File file = chooser.showDialog(mainGridPane.getScene().getWindow());
+        if(file != null){
+            System.out.println(file.getPath());
+        } else {
+            System.out.println("chooser not doing what was expected");
+        }
+
     }
 }
