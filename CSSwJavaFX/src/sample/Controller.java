bc4f384058f5a5dc7ed011c10d8f8c2bccc0426b
KevinW <bird831205@hotmail.com>
2019-11-05 18:03:54 -0500

    playing with some effects

--- CSSwJavaFX/src/sample/Controller.java
+++ CSSwJavaFX/src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+    @FXML
+    private Label label1;
+
+    public void initialize(){
+//        label1.setScaleX(2.0);
+//        label1.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseOver(){
+        label1.setScaleX(2.0);
+        label1.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit(){
+        label1.setScaleX(1.0);
+        label1.setScaleY(1.0);
+    }
 }
