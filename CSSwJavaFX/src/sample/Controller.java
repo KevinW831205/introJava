50f7b9392594d6ae81cd06410e42923ee6f4866f
KevinW <bird831205@hotmail.com>
2019-11-06 22:21:49 -0500

    webview display webpage

--- CSSwJavaFX/src/sample/Controller.java
+++ CSSwJavaFX/src/sample/Controller.java
@@ -6,6 +6,8 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
@@ -23,6 +25,8 @@ public class Controller {
     private Button button4;
     @FXML
     private GridPane mainGridPane;
+    @FXML
+    private WebView webView;
 
     public void initialize() {
 //        label1.setScaleX(2.0);
@@ -94,4 +98,11 @@ public class Controller {
             e.printStackTrace();
         }
     }
+
+    @FXML
+    public void handleWebViewLinkClick(){
+        WebEngine engine = webView.getEngine();
+        engine.load("http://www.google.com");
+    }
+
 }
