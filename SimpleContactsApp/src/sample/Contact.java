34f03fa13840365dee23dc4704c532a326cbd251
KevinW <bird831205@hotmail.com>
2019-11-07 14:34:32 -0500

    modifying contact class to have fields as SimpleStringProperty

--- SimpleContactsApp/src/sample/Contact.java
+++ SimpleContactsApp/src/sample/Contact.java
@@ -1,54 +1,66 @@
 package sample;
 
-public class Contact {
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String notes;
+import javafx.beans.property.SimpleStringProperty;
 
-    public Contact(String firstName, String lastName, String phoneNumber, String notes) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.notes = notes;
-    }
+public class Contact {
+    private SimpleStringProperty firstName;
+    private SimpleStringProperty lastName;
+    private SimpleStringProperty phoneNumber;
+    private SimpleStringProperty notes;
+
 
     public Contact() {
-        this.firstName="";
-        this.lastName = "";
-        this.phoneNumber = "";
-        this.notes = "";
+        this.firstName= new SimpleStringProperty();
+        this.lastName = new SimpleStringProperty();
+        this.phoneNumber = new SimpleStringProperty();
+        this.notes = new SimpleStringProperty();
     }
 
     public String getFirstName() {
+        return firstName.get();
+    }
+
+    public SimpleStringProperty firstNameProperty() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        this.firstName.set(firstName);
     }
 
     public String getLastName() {
+        return lastName.get();
+    }
+
+    public SimpleStringProperty lastNameProperty() {
         return lastName;
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        this.lastName.set(lastName);
     }
 
     public String getPhoneNumber() {
+        return phoneNumber.get();
+    }
+
+    public SimpleStringProperty phoneNumberProperty() {
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        this.phoneNumber.set(phoneNumber);
     }
 
     public String getNotes() {
+        return notes.get();
+    }
+
+    public SimpleStringProperty notesProperty() {
         return notes;
     }
 
     public void setNotes(String notes) {
-        this.notes = notes;
+        this.notes.set(notes);
     }
 }
