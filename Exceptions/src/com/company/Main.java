48e9df9a66d318d930774840add06aaea6755309
KevinW <bird831205@hotmail.com>
2019-11-07 19:29:21 -0500

    more exception

--- Exceptions/src/com/company/Main.java
+++ Exceptions/src/com/company/Main.java
@@ -1,15 +1,55 @@
 package com.company;
 
 import java.io.IOError;
+import java.nio.file.LinkPermission;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int x =9;
-        int y=0;
-        System.out.println(divideLBYL(x,y));
-        System.out.println(divideEAFP(x,y));
+//        int x =9;
+//        int y=0;
+//        System.out.println(divideLBYL(x,y));
+//        System.out.println(divideEAFP(x,y));
+
+//        int x = getInt();
+//        int x = getIntLBYL();
+        int x =getIntEAFP();
+        System.out.println("x: "+x);
+    }
+
+    private static int getInt(){
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+
+    private static int getIntLBYL(){
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Enter a integer");
+        String input = s.next();
+        for(int i=0; i<input.length(); i++){
+            if(!Character.isDigit(input.charAt(i))){
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid){
+            return Integer.parseInt(input);
+        }
+        return 0;
+    }
+
+    private static int getIntEAFP(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer");
+        try {
+            return s.nextInt();
+        } catch (InputMismatchException e){
+            return 0;
+        }
     }
 
     private static int divideLBYL(int x, int y){
