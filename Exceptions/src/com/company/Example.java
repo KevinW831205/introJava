b4e53144852c283e7bf31a4425bc12f12dad6ce8
KevinW <bird831205@hotmail.com>
2019-11-08 19:01:38 -0500

    multicatch

--- Exceptions/src/com/company/Example.java
+++ Exceptions/src/com/company/Example.java
@@ -6,8 +6,14 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e){
+            System.out.println(e.toString());
+            System.out.println("unable to perform division");
+        }
+
     }
 
     private static int divide(){
@@ -21,20 +27,25 @@ public class Example {
 //        } catch (NoSuchElementException e){
 //            x = getInt();
 //        }
+//        int x,y;
+//
+//        try{
+//            x = getInt();
+//            y =getInt();
+//            System.out.println("x is "+x+", y is "+y);
+//            return x/y;
+//        } catch (NoSuchElementException e){
+//            throw new ArithmeticException("no suitable input");
+//        } catch (ArithmeticException e){
+//            throw new ArithmeticException("attempt to divide by zero");
+//        }
         int x,y;
-
-        try{
-            x = getInt();
-            y =getInt();
-        } catch (NoSuchElementException e){
-            throw new ArithmeticException("no suitable input");
-        }
+        x = getInt();
+        y =getInt();
         System.out.println("x is "+x+", y is "+y);
-        try {
-            return x/y;
-        } catch (ArithmeticException e){
-            throw new ArithmeticException("attempt to divide by zero");
-        }
+        return x/y;
+
+
     }
 
     private static int getInt(){
@@ -45,7 +56,7 @@ public class Example {
                 return s.nextInt();
             } catch (InputMismatchException e){
                 s.nextLine();
-                System.out.println("Please enter an integer");
+                System.out.println("Invalid, Please enter an integer");
             }
         }
     }
