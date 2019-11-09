832e99426e93da2f6d899f5a9f51c751ecd086a4
KevinW <bird831205@hotmail.com>
2019-11-08 20:27:15 -0500

    reading user input

--- JavaIO/src/com/company/Main.java
+++ JavaIO/src/com/company/Main.java
@@ -28,6 +28,36 @@ public class Main {
 
         int loc = 1;
         while(true) {
+            System.out.println(locations.get(loc).getDescription());
+
+            if(loc == 0) {
+                break;
+            }
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for(String exit: exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word: words) {
+                    if(vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
+
+            } else {
+                System.out.println("You cannot go in that direction");
+            }
         }
 
     }
