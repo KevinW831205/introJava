628942ab22fe92c9c6168b2903731fc66791bc6e
KevinW <bird831205@hotmail.com>
2019-11-10 15:56:16 -0500

    reading from binary

--- JavaIO/src/com/company/Locations.java
+++ JavaIO/src/com/company/Locations.java
@@ -54,6 +54,32 @@ public class Locations implements Map<Integer, Location> {
     }
 
     static {
+
+
+        //Binary data
+
+        try(DataInputStream locFile = new DataInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))){
+            while (true){
+                Map<String,Integer> exits = new LinkedHashMap<>();
+                int locID = locFile.readInt();
+                String description = locFile.readUTF();
+                int numExits =locFile.readInt();
+                System.out.println("Read location "+locID+" : "+description);
+                System.out.println("Found " + numExits +" exits");
+                for(int i=0; i<numExits; i++){
+                    String direction = locFile.readUTF();
+                    int destination = locFile.readInt();
+                    exits.put(direction,destination);
+                    System.out.println("\t\t"+direction+","+destination);
+                }
+                locations.put(locID, new Location(locID,description,exits));
+            }
+        } catch (IOException io){
+            System.out.println("IO exception");
+        }
+
+
+        /*
         // try with resource
         try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations_big.txt")))) {
             scanner.useDelimiter(",");
@@ -83,6 +109,7 @@ public class Locations implements Map<Integer, Location> {
         } catch (IOException e) {
             e.printStackTrace();
         }
+         */
 
 
 //        Scanner scanner =null;
