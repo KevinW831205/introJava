ee9903e0d87aafc478295bca1f5822beb6d6505f
KevinW <bird831205@hotmail.com>
2019-11-10 20:00:48 -0500

    spinwords practice

---
+++ Exercieses/src/com/company/SpinWords.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class SpinWords {
+    public static void main(String[] args) {
+
+        System.out.println(spinWords("Welcome"));
+        System.out.println(spinWords("Hey fellow warriors"));
+
+    }
+
+    public static String spinWords(String sentence) {
+
+        String[] words = sentence.split(" ");
+        String result = "";
+
+        for (String word : words) {
+            StringBuilder s = new StringBuilder(word);
+            if (word.length() >= 5) {
+                result += s.reverse() +" ";
+            } else {
+                result += s +" ";
+            }
+        }
+
+        return result.trim();
+    }
+
+}
