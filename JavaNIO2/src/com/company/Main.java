bb2a655baf1e34dde7ebbd17eb0298f40449baa7
KevinW <bird831205@hotmail.com>
2019-11-13 19:22:20 -0500

    writing bytes with NIO

--- JavaNIO2/src/com/company/Main.java
+++ JavaNIO2/src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOError;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.*;
 import java.util.List;
@@ -10,17 +12,27 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	    try{
+        try (FileOutputStream binFile = new FileOutputStream("data.dat");
+             FileChannel binChannel = binFile.getChannel();
+             ) {
+            //writing text
 //            FileInputStream file = new FileInputStream("Data.txt");
 //            FileChannel channel = file.getChannel();
 
-            Path dataPath = FileSystems.getDefault().getPath("Data.txt");
-            Files.write(dataPath, "\nline4".getBytes("UTF-8"), StandardOpenOption.APPEND);
+//            Path dataPath = FileSystems.getDefault().getPath("Data.txt");
+//            Files.write(dataPath, "\nline4".getBytes("UTF-8"), StandardOpenOption.APPEND);
+//
+//            List<String> lines = Files.readAllLines(dataPath);
+//            for(String line : lines){
+//                System.out.println(line);
+//            }
 
-            List<String> lines = Files.readAllLines(dataPath);
-            for(String line : lines){
-                System.out.println(line);
-            }
+            //writing binary with try with resource
+            byte[] outputBytes = "Hello World!".getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            int numBytes = binChannel.write(buffer);
+            System.out.println("numBytes written was: " + numBytes);
+
 
         } catch (IOException e) {
             e.printStackTrace();
