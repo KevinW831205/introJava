140f10fc961dc1c0b4ef582a9c7a8d1b9ce826f4
KevinW <bird831205@hotmail.com>
2019-11-13 20:56:30 -0500

    retrieving data

--- JavaNIO2/src/com/company/Main.java
+++ JavaNIO2/src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOError;
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.*;
@@ -45,7 +42,17 @@ public class Main {
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
 
+            // reading
 
+            RandomAccessFile ra = new RandomAccessFile("data.dat","rwd");
+            byte[] b = new byte[outputBytes.length];
+            ra.read(b);
+            System.out.println(new String(b));
+
+            long int1 = ra.readInt();
+            long int2 = ra.readInt();
+            System.out.println(int1);
+            System.out.println(int2);
 
         } catch (IOException e) {
             e.printStackTrace();
