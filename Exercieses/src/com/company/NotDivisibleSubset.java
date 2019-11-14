cb143aa1206525b434125230aeae47682972a5d4
KevinW <bird831205@hotmail.com>
2019-11-14 06:43:58 -0500

    adjusting initial nondivisible to account for the number looking at

--- Exercieses/src/com/company/NotDivisibleSubset.java
+++ Exercieses/src/com/company/NotDivisibleSubset.java
@@ -13,7 +13,19 @@ public class NotDivisibleSubset {
         input1.add(2);
         input1.add(4);
 
-        System.out.println( nonDivisibleSubset(4,input1));
+        List<Integer> input2 = new ArrayList<>();
+        input2.add(19);
+        input2.add(10);
+        input2.add(12);
+        input2.add(10);
+        input2.add(24);
+        input2.add(25);
+        input2.add(22);
+
+
+
+        System.out.println( nonDivisibleSubset(3,input1));
+        System.out.println(nonDivisibleSubset(4, input2));
     }
 
     public static int nonDivisibleSubset(int k, List<Integer> s) {
@@ -21,11 +33,13 @@ public class NotDivisibleSubset {
 
         int largestNonDivisible = 0;
         for(int i =0; i<s.size(); i++){
-            int nonDivisibles = 0;
+            int nonDivisibles = 1;
+            System.out.println("current index: "+i);
             for(int j=0; j<s.size(); j++){
                 if(i != j){
                     int sum = s.get(i) + s.get(j);
                     if(sum%k != 0) {
+                        System.out.println(s.get(i)+","+s.get(j));
                         nonDivisibles++;
                     }
                 }
