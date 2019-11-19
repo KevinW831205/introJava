4c3b7abd695ffac3cdf218710d8d7e33600fbb86
KevinW <bird831205@hotmail.com>
2019-11-19 08:46:31 -0500

    finding balls per container and balls per type

--- Exercieses/src/com/company/OrganizeContainer.java
+++ Exercieses/src/com/company/OrganizeContainer.java
@@ -3,30 +3,52 @@ package com.company;
 public class OrganizeContainer {
 
     public static void main(String[] args) {
-
+        int[][] test = new int[][]{new int[]{0,2,1}, new int[]{1,1,1}, new int[]{2,0,0}};
+        System.out.println(organizingContainers(test));
     }
 
     static String organizingContainers(int[][] container) {
 
-        for(int i=0; i<container.length; i++){
-            int horizontal = 0;
-            int vertical = 0;
-            for(int j=0; j<container.length;j++){
-                if(i != j){
-                    horizontal+= container[i][j];
-                    vertical += container[j][i];
-                }
-            }
+        int[] ballsPerType = new int[container.length];
+        int[] ballsPerContainer = new int[container.length];
 
-            if(horizontal != vertical){
-                return "impossible";
+
+
+
+        for(int i =0; i<container.length; i++){
+            for(int j =0; j<container.length; j++){
+                ballsPerContainer[j] += container[i][j];
+                ballsPerType[j] += container[j][i];
             }
         }
 
+        System.out.println("per container");
+        for(int s : ballsPerContainer){
+            System.out.println(s);
+        }
+        System.out.println("per type");
 
+        for(int s : ballsPerType ){
+            System.out.println(s);
+        }
 
+        return "";
 
-        return "possible";
+//        for(int i=0; i<container.length; i++){
+//            int horizontal = 0;
+//            int vertical = 0;
+//            for(int j=0; j<container.length;j++){
+//                if(i != j){
+//                    horizontal+= container[i][j];
+//                    vertical += container[j][i];
+//                }
+//            }
+//
+//            if(horizontal != vertical){
+//                return "impossible";
+//            }
+//        }
+//        return "possible";
     }
 
 }
