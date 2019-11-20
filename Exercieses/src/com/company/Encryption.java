55581f0f0330e76ea2c06744796a15f9f8be5efa
KevinW <bird831205@hotmail.com>
2019-11-20 09:13:40 -0500

    fixing space bug

--- Exercieses/src/com/company/Encryption.java
+++ Exercieses/src/com/company/Encryption.java
@@ -3,6 +3,7 @@ package com.company;
 public class Encryption {
     public static void main(String[] args) {
         System.out.println(encryption("haveaniceday"));
+        System.out.println(encryption("feedthedog"));
     }
 
     static String encryption(String s) {
@@ -14,39 +15,36 @@ public class Encryption {
         int rowPos = 0;
         int colPos = 0;
 
+        System.out.println(rows);
+
         encrypted[rowPos] = new char[cols];
 
         for (int i = 0; i < s.length(); i++) {
             encrypted[rowPos][colPos] = s.charAt(i);
 //            System.out.println("00"+encrypted[0][0]);
 //            System.out.println("01"+encrypted[0][1]);
-            System.out.println(encrypted[rowPos][colPos]);
+//            System.out.println(encrypted[rowPos][colPos]);
             colPos++;
 
             if (colPos == cols) {
                 colPos = 0;
                 rowPos++;
-                System.out.println("rowpos");
-                if(rowPos < rows){
+//                System.out.println("rowpos");
+                if (rowPos < rows) {
                     encrypted[rowPos] = new char[cols];
                 }
             }
         }
 
         String eResult = "";
-        for(int j=0; j<cols; j++){
-            for(int i=0; i<rowPos;i++){
-                System.out.println(encrypted[i][j]);
-                eResult += encrypted[i][j];
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < rows; i++) {
+                if (encrypted[i][j] != '\u0000') {
+                    eResult += encrypted[i][j];
+                }
             }
-            eResult+=" ";
+            eResult += " ";
         }
-
-
-
-
-
-
         return eResult;
     }
 
