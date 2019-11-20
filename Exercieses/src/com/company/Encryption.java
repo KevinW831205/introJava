bb04ffe7e51b36b2c036e09184ffbd5645676e5c
KevinW <bird831205@hotmail.com>
2019-11-19 19:40:15 -0500

    attempt to create array

--- Exercieses/src/com/company/Encryption.java
+++ Exercieses/src/com/company/Encryption.java
@@ -6,16 +6,35 @@ public class Encryption {
     }
 
     static String encryption(String s) {
-        int row = (int) Math.floor(Math.sqrt(s.length()));
-        int col = (int) Math.ceil(Math.sqrt(s.length()));
+        int rows = (int) Math.floor(Math.sqrt(s.length()));
+        int cols = (int) Math.ceil(Math.sqrt(s.length()));
 
-        char[][] encrypted = new char[row][];
+        char[][] encrypted = new char[rows][];
 
-        encrypted[0] = new char[col];
+        int rowPos = 0;
+        int colPos = 0;
 
-        for(int i=0; i<s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println("i" + i);
+            System.out.println("colpos " + colPos);
+            System.out.println("rowpos " + rowPos);
+            encrypted[rowPos] = new char[cols];
+            encrypted[rowPos][colPos] = s.charAt(i);
             System.out.println(s.charAt(i));
+            colPos++;
 
+            if (colPos == cols) {
+                colPos = 0;
+                rowPos++;
+            }
+        }
+
+        System.out.println(encrypted[0][0]);
+
+        for (int i = 0; i < encrypted.length; i++) {
+            for (int j = 0; j < encrypted.length; j++) {
+                System.out.println(encrypted[i][j]);
+            }
         }
 
         return "";
