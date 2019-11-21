a005d7b9e532e5ed54ec211a0269f0c010a709b6
KevinW <bird831205@hotmail.com>
2019-11-21 12:33:20 -0500

    bug fix

--- Exercieses/src/com/company/BiggerGreater.java
+++ Exercieses/src/com/company/BiggerGreater.java
@@ -8,14 +8,13 @@ public class BiggerGreater {
 //        System.out.println(biggerIsGreater("bb"));
 //        System.out.println(biggerIsGreater("hefg"));
 //        System.out.println(biggerIsGreater("dhck"));
-//        System.out.println(biggerIsGreater("dkhc"));
-        System.out.println(biggerIsGreater("abcfdda"));
+        System.out.println(biggerIsGreater("dkhc"));
+//        System.out.println(biggerIsGreater("abcfdda"));
 
     }
 
     static String biggerIsGreater(String w) {
 
-        int indexOfNotIncreasing = -1;
         String result = "";
 
         for (int i = w.length() - 2; i >= 0; i--) {
@@ -24,40 +23,34 @@ public class BiggerGreater {
                 String left = w.substring(0, i);
                 String right = w.substring(i);
 
-                if (left.length() != 0) {
-                    char pivot = right.charAt(0);
+                System.out.println("left " + left);
+                System.out.println("right " + right);
 
-                    for (int j = 0; j < right.length(); j++) {
-                        if (right.charAt(j) < pivot) {
-                            System.out.println("in if");
-                            StringBuilder rightSB = new StringBuilder(right);
-                            char temp = rightSB.charAt(j-1);
-                            System.out.println("temp"+temp );
-                            rightSB.setCharAt(j-1, pivot);
-                            rightSB.setCharAt(0, temp);
+                char pivot = right.charAt(0);
 
-                            result = left + rightSB.charAt(0);
+                for (int j = 0; j < right.length(); j++) {
+                    if (right.charAt(j) < pivot) {
+                        StringBuilder rightSB = new StringBuilder(right);
+                        char temp = rightSB.charAt(j - 1);
+                        rightSB.setCharAt(j - 1, pivot);
+                        rightSB.setCharAt(0, temp);
 
-                            rightSB.delete(0,1);
-                            right = rightSB.reverse().toString();
-                            return result + right;
-                        }
+                        result = left + rightSB.charAt(0);
 
+                        rightSB.delete(0, 1);
+                        right = rightSB.reverse().toString();
+                        return result + right;
                     }
-                    StringBuilder rightSB = new StringBuilder(right);
-                    return left + rightSB.reverse().toString();
 
-
-                } else {
-                    result = new StringBuilder(right).reverse().toString();
-                    return result;
                 }
+                StringBuilder rightSB = new StringBuilder(right);
+                return left + rightSB.reverse().toString();
+
 
             }
         }
 
         return "no answer";
-
 
     }
 
