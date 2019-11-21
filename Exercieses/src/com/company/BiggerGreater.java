7e548a0f9b3382be2bd704aaa9b3ac1eb8b8c86a
KevinW <bird831205@hotmail.com>
2019-11-21 15:14:43 -0500

    reading from end

--- Exercieses/src/com/company/BiggerGreater.java
+++ Exercieses/src/com/company/BiggerGreater.java
@@ -8,7 +8,8 @@ public class BiggerGreater {
 //        System.out.println(biggerIsGreater("bb"));
 //        System.out.println(biggerIsGreater("hefg"));
 //        System.out.println(biggerIsGreater("dhck"));
-        System.out.println(biggerIsGreater("dkhc"));
+//        System.out.println(biggerIsGreater("dkhc"));
+        System.out.println(biggerIsGreater("abdc"));
 //        System.out.println(biggerIsGreater("abcfdda"));
 
     }
@@ -17,22 +18,20 @@ public class BiggerGreater {
 
         String result = "";
 
-        for (int i = w.length() - 2; i >= 0; i--) {
-            if (w.charAt(i) < w.charAt(i + 1)) {
+        for (int i = w.length() - 1; i > 0; i--) {
+            if (w.charAt(i) > w.charAt(i - 1)) {
 
-                String left = w.substring(0, i);
-                String right = w.substring(i);
+                String left = w.substring(0, i-1);
+                String right = w.substring(i-1);
 
-                System.out.println("left " + left);
-                System.out.println("right " + right);
 
                 char pivot = right.charAt(0);
 
-                for (int j = 0; j < right.length(); j++) {
-                    if (right.charAt(j) < pivot) {
+                for (int j = right.length()-1; j >= 0; j--) {
+                    if (right.charAt(j)> pivot) {
                         StringBuilder rightSB = new StringBuilder(right);
-                        char temp = rightSB.charAt(j - 1);
-                        rightSB.setCharAt(j - 1, pivot);
+                        char temp = rightSB.charAt(j );
+                        rightSB.setCharAt(j , pivot);
                         rightSB.setCharAt(0, temp);
 
                         result = left + rightSB.charAt(0);
@@ -43,10 +42,10 @@ public class BiggerGreater {
                     }
 
                 }
+
+
                 StringBuilder rightSB = new StringBuilder(right);
                 return left + rightSB.reverse().toString();
-
-
             }
         }
 
