e07509c9416b1255a09126905af1adc77d1008bc
KevinW <bird831205@hotmail.com>
2019-11-21 09:33:25 -0500

    some implementation

--- Exercieses/src/com/company/BiggerGreater.java
+++ Exercieses/src/com/company/BiggerGreater.java
@@ -15,40 +15,41 @@ public class BiggerGreater {
 
     static String biggerIsGreater(String w) {
 
-        boolean foundGreater = false;
         int indexOfNotIncreasing = -1;
         String result = "";
 
         for (int i = w.length() - 2; i >= 0; i--) {
-            if (w.charAt(i) < w.charAt(i+1)){
-                foundGreater = true;
-                indexOfNotIncreasing = i;
+            if (w.charAt(i) < w.charAt(i + 1)) {
+                indexOfNotIncreasing = i+1;
 
-                String left = w.substring(0,indexOfNotIncreasing);
+                String left = w.substring(0, indexOfNotIncreasing);
                 String right = w.substring(indexOfNotIncreasing);
+                
+                if (!left.equals("")) {
+                    char pivot = left.charAt(left.length() - 1);
 
+                    for(int j=0; j<right.length(); j++){
+                        if(right.charAt(j) < pivot){
+                            left = left.substring(0, left.length()-1) + right.charAt(j-1);
 
-                if(!left.equals("")){
-                    char pivot = left.charAt(left.length()-1);
-                    System.out.println(pivot);
+                            StringBuilder rightSB = new StringBuilder(right);
+                            rightSB.setCharAt(j-1, pivot);
+                            right = rightSB.reverse().toString();
+
+                            return left+right;
+                        }
+                    }
 
                 } else {
-
+                    result = new StringBuilder(right).reverse().toString();
+                    return result;
                 }
                 break;
             }
         }
 
-        if (foundGreater) {
+        return "no answer";
 
-
-
-        } else {
-            return "no answer";
-        }
-
-
-        return result;
 
     }
 
