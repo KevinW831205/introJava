c4464a3443f0ae7b9ec698d42efb19cffc0d6ecd
KevinW <bird831205@hotmail.com>
2019-11-22 18:39:31 -0500

    reading text file in current directory

--- JavaPaths/src/com/company/Main.java
+++ JavaPaths/src/com/company/Main.java
@@ -1,9 +1,33 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Path path = FileSystems.getDefault().getPath("WorkingDirectory.txt");
+        printFile(path);
+
+
+    }
+
+    private static void printFile(Path path) {
+
+        try (BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 
