f39402675f3fa56dd66fbee5d715212ddabd07eb
KevinW <bird831205@hotmail.com>
2019-11-22 18:43:51 -0500

    reading text outside of directory

--- JavaPaths/src/com/company/Main.java
+++ JavaPaths/src/com/company/Main.java
@@ -2,10 +2,7 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 public class Main {
 
@@ -14,6 +11,8 @@ public class Main {
         Path path = FileSystems.getDefault().getPath("WorkingDirectory.txt");
         printFile(path);
         Path filePath = FileSystems.getDefault().getPath("Files", "SubDirectory.txt");
+        printFile(filePath);
+        filePath = Paths.get("K:\\Java\\OutThere.txt");
         printFile(filePath);
 
 
