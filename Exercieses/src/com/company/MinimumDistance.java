147a1c39ceb8d8195bd6ec97ae06ed9fa0a7a6a7
KevinW <bird831205@hotmail.com>
2019-11-27 18:01:34 -0500

    minimum distance setup

---
+++ Exercieses/src/com/company/MinimumDistance.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinimumDistance {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(minimumDistances(new int[]{3,2,1,2,3}));
+        System.out.println(minimumDistances(new int[]{7,1,3,4,1,7}));
+
+    }
+
+    static int minimumDistances(int[] a) {
+
+        Map<Integer, Integer> distanceMap = new HashMap<Integer, Integer>();
+
+        for(int i=0; i<a.length; i++){
+            if()
+        }
+    }
+
+}
