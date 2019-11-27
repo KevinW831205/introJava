4c30f838590fe5460517df38a7c5b8d8523d7734
KevinW <bird831205@hotmail.com>
2019-11-27 18:11:15 -0500

    something working

--- Exercieses/src/com/company/MinimumDistance.java
+++ Exercieses/src/com/company/MinimumDistance.java
@@ -8,8 +8,8 @@ public class MinimumDistance {
 
     public static void main(String[] args) {
 
-        System.out.println(minimumDistances(new int[]{3,2,1,2,3}));
-        System.out.println(minimumDistances(new int[]{7,1,3,4,1,7}));
+        System.out.println(minimumDistances(new int[]{3, 2, 1, 2, 3}) + "expects 2");
+        System.out.println(minimumDistances(new int[]{7, 1, 3, 4, 1, 7}) + "expects 3");
 
     }
 
@@ -19,11 +19,21 @@ public class MinimumDistance {
 
         int minDistance = -1;
 
-        for(int i=0; i<a.length; i++){
-            if(indexMap.containsKey(a[i])){
+        for (int i = 0; i < a.length; i++) {
+            if (indexMap.containsKey(a[i])) {
+                int index1 = indexMap.get(a[i]);
+                int distance = i - index1;
+
+                if (minDistance == -1) {
+                    minDistance = distance;
+                } else if (distance<minDistance){
+                    minDistance = distance;
+                }
+
+                indexMap.replace(a[i], i);
 
             } else {
-                indexMap.put(a[i],i);
+                indexMap.put(a[i], i);
             }
 
         }
