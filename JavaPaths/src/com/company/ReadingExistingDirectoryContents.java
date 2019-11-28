29f095e7ac3301cf749e80283b6c0a42ee73558f
KevinW <bird831205@hotmail.com>
2019-11-28 16:50:04 -0500

    glob filters

--- JavaPaths/src/com/company/ReadingExistingDirectoryContents.java
+++ JavaPaths/src/com/company/ReadingExistingDirectoryContents.java
@@ -1,22 +1,27 @@
 package com.company;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 public class ReadingExistingDirectoryContents {
 
     public static void main(String[] args) {
+
+//        DirectoryStream<Path> filter = new DirectoryStream.Filter<Path>(){
+//            @Override
+//            public boolean accept(Path entry) throws IOException {
+//                return Files.isRegularFile(entry);
+//            }
+//        }
+
         Path directory = FileSystems.getDefault().getPath("Examples\\Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory,"*.dat")) {
 
             for (Path file : contents){
                 System.out.println(file.getFileName());
             }
 
-        } catch (IOException e) {
+        } catch (IOException | DirectoryIteratorException e) {
             System.out.println(e.getMessage());
         }
 
