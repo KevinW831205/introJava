3c1f335b30c2e63ad256b34bb142ec2370cbc1e1
KevinW <bird831205@hotmail.com>
2019-11-28 16:54:39 -0500

    directorystream.filter

--- JavaPaths/src/com/company/ReadingExistingDirectoryContents.java
+++ JavaPaths/src/com/company/ReadingExistingDirectoryContents.java
@@ -7,17 +7,17 @@ public class ReadingExistingDirectoryContents {
 
     public static void main(String[] args) {
 
-//        DirectoryStream<Path> filter = new DirectoryStream.Filter<Path>(){
-//            @Override
-//            public boolean accept(Path entry) throws IOException {
-//                return Files.isRegularFile(entry);
-//            }
-//        }
+        DirectoryStream.Filter<Path> filter =
+                new DirectoryStream.Filter<Path>() {
+                    public boolean accept(Path entry) throws IOException {
+                        return (Files.isRegularFile(entry));
+                    }
+                };
 
         Path directory = FileSystems.getDefault().getPath("Examples\\Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory,"*.dat")) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
 
-            for (Path file : contents){
+            for (Path file : contents) {
                 System.out.println(file.getFileName());
             }
 
