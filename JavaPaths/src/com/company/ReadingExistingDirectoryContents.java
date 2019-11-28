4fd38b0815f84bd9589696e7c954f9a2af606cc4
KevinW <bird831205@hotmail.com>
2019-11-28 16:56:28 -0500

    lambda expression for filter

--- JavaPaths/src/com/company/ReadingExistingDirectoryContents.java
+++ JavaPaths/src/com/company/ReadingExistingDirectoryContents.java
@@ -7,12 +7,14 @@ public class ReadingExistingDirectoryContents {
 
     public static void main(String[] args) {
 
-        DirectoryStream.Filter<Path> filter =
-                new DirectoryStream.Filter<Path>() {
-                    public boolean accept(Path entry) throws IOException {
-                        return (Files.isRegularFile(entry));
-                    }
-                };
+//        DirectoryStream.Filter<Path> filter =
+//                new DirectoryStream.Filter<Path>() {
+//                    public boolean accept(Path entry) throws IOException {
+//                        return (Files.isRegularFile(entry));
+//                    }
+//                };
+
+        DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
 
         Path directory = FileSystems.getDefault().getPath("Examples\\Dir2");
         try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
