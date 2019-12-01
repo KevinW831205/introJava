740a0e6c319c5896c69dcfe957ec925e2bced60e
KevinW <bird831205@hotmail.com>
2019-12-01 12:53:26 -0500

    removing thread interference

--- Counter/src/com/company/Main.java
+++ Counter/src/com/company/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Countdown countdown = new Countdown();
+        Countdown countdown2 = new Countdown();
 
         CountDownThread cdt1 = new CountDownThread(countdown);
         cdt1.setName("Thread 1");
-        CountDownThread cdt2 = new CountDownThread(countdown);
+        CountDownThread cdt2 = new CountDownThread(countdown2);
         cdt2.setName("Thread 2");
 
         cdt1.start();
