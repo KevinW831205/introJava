7b9b1e951efefe5c761c824cc30ca8e69d07578b
KevinW <bird831205@hotmail.com>
2019-12-01 13:56:31 -0500

    writer class

---
+++ Messages/src/com/company/Writer.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Random;
+
+public class Writer implements Runnable {
+    private Message message;
+
+    private Writer(Message message){
+        this.message = message;
+    }
+
+    public void run(){
+        String messages[] = {
+                "some sentence one",
+                "some sentence two",
+                "some sentence three",
+                "some sentence four"
+        };
+
+        Random random = new Random();
+
+        for(int i=0; i<messages.length;i++){
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+        message.write("Finished");
+    }
+}
