2d6c67e82ebdb3d6f6a73b2f96e860bf228c06f3
KevinW <bird831205@hotmail.com>
2019-11-30 22:00:55 -0500

    multiple threads

--- Counter/src/com/company/Main.java
+++ Counter/src/com/company/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Countdown countdown = new Countdown();
 
+        CountDownThread cdt1 = new CountDownThread(countdown);
+        cdt1.setName("Thread 1");
+        CountDownThread cdt2 = new CountDownThread(countdown);
+        cdt2.setName("Thread 2");
+
+        cdt1.start();
+        cdt2.start();
 
     }
 
