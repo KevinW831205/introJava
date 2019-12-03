6b9cd2e7b4c286326c05d7c50a4c2fe8e9a523f1
KevinW <bird831205@hotmail.com>
2019-12-03 07:13:33 -0500

    debbuging edge case where problem%no=0

--- Exercieses/src/com/company/LisaWorkbook.java
+++ Exercieses/src/com/company/LisaWorkbook.java
@@ -4,23 +4,32 @@ public class LisaWorkbook {
 
     public static void main(String[] args) {
 
+        System.out.println(workbook(5, 3, new int[]{4, 2, 6, 1, 10}));
     }
 
     static int workbook(int n, int k, int[] arr) {
         // n chapters, k problems/page, arr problems/chapter
         int pageNumber = 1;
         int result = 0;
+        int problemsOnPage = 0;
 
-        for(int i=1; i<=arr.length; i++){
+
+        for (int i = 0; i < arr.length; i++) {
             int problemsPerChapter = arr[i];
-            for(int problem=1; problem<=problemsPerChapter; problem++){
-                if(problem == pageNumber){
+            problemsOnPage = 0;
+
+            for (int problemNo = 1; problemNo <= problemsPerChapter; problemNo++) {
+                problemsOnPage++;
+                if (problemNo == pageNumber) {
                     result++;
                 }
-                if(problem == k){
+                if (problemsOnPage == k && problemNo != problemsPerChapter) {
                     pageNumber++;
+                    problemsOnPage = 0;
                 }
+
             }
+
             pageNumber++;
         }
         return result;
