85d46e408ca29b14a3b528c43278a75e6e9230b3
KevinW <bird831205@hotmail.com>
2019-12-07 16:40:45 -0500

    synchronizing producer

--- ProducerConsumer/src/com/company/MyConsumer.java
+++ ProducerConsumer/src/com/company/MyConsumer.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import static com.company.Main.EOF;
 
-public class MyConsumer implements Runnable{
+public class MyConsumer implements Runnable {
     private List<String> buffer;
     private String color;
 
@@ -15,15 +15,17 @@ public class MyConsumer implements Runnable{
 
     @Override
     public void run() {
-        while (true){
-            if(buffer.isEmpty()){
-                continue;
-            }
-            if(buffer.get(0).equals(EOF)){
-                System.out.println(color+"Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed "+buffer.remove(0));
+        while (true) {
+            synchronized (buffer) {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if (buffer.get(0).equals(EOF)) {
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
             }
         }
     }
