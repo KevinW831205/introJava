39cdc1ae0a4f51dce9bbe3899d4438b4a34c0ce1
KevinW <bird831205@hotmail.com>
2019-12-07 19:26:31 -0500

    replace synchronize with Reentrant loci

--- ProducerConsumer/src/com/company/MyConsumer.java
+++ ProducerConsumer/src/com/company/MyConsumer.java
@@ -1,32 +1,35 @@
 package com.company;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.company.Main.EOF;
 
 public class MyConsumer implements Runnable {
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    public MyConsumer(List<String> buffer, String color) {
+    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
 
     @Override
     public void run() {
         while (true) {
-            synchronized (buffer) {
-                if (buffer.isEmpty()) {
-                    continue;
-                }
-                if (buffer.get(0).equals(EOF)) {
-                    System.out.println(color + "Exiting");
-                    break;
-                } else {
-                    System.out.println(color + "Removed " + buffer.remove(0));
-                }
+            bufferLock.lock();
+            if (buffer.isEmpty()) {
+                continue;
             }
+            if (buffer.get(0).equals(EOF)) {
+                System.out.println(color + "Exiting");
+                break;
+            } else {
+                System.out.println(color + "Removed " + buffer.remove(0));
+            }
+            bufferLock.unlock();
         }
     }
 }
--- ProducerConsumer/src/com/company/MyProducer.java
+++ ProducerConsumer/src/com/company/MyProducer.java
@@ -2,37 +2,41 @@ package com.company;
 
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MyProducer implements Runnable {
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    public MyProducer(List<String> buffer, String color) {
+
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
 
     @Override
     public void run() {
         Random random = new Random();
-        String[] nums =  {"1","2","3","4","5"};
+        String[] nums = {"1", "2", "3", "4", "5"};
 
-        for(String num : nums){
+        for (String num : nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                synchronized (buffer){
-                    buffer.add(num);
-                }
+                bufferLock.lock();
+                buffer.add(num);
+                bufferLock.unlock();
 
                 Thread.sleep(random.nextInt(1000));
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 System.out.println("Producer interrupted");
             }
         }
 
         System.out.println(color + "Adding EOF and exiting...");
-        synchronized (buffer){
-            buffer.add("End of File");
-        }
+        bufferLock.lock();
+        buffer.add("End of File");
+        bufferLock.unlock();
     }
 }
