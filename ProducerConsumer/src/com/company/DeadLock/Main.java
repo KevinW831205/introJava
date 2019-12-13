4729dab820815f12b9405b1a610200c2b64786ae
KevinW <bird831205@hotmail.com>
2019-12-12 20:30:09 -0500

    fixing deadlocks by obtaining lock in same order

--- ProducerConsumer/src/com/company/DeadLock/Main.java
+++ ProducerConsumer/src/com/company/DeadLock/Main.java
@@ -31,20 +31,20 @@ public class Main {
 
     private static class Thread2 extends Thread{
         public void run(){
-            synchronized (lock2){
-                System.out.println("Thread2 has lock2");
+            synchronized (lock1){
+                System.out.println("Thread2 has lock1");
                 try{
                     Thread.sleep(100);
                 }catch (InterruptedException e){
 
                 }
-                System.out.println("Thread2 waiting for lock1");
-                synchronized (lock1){
+                System.out.println("Thread2 waiting for lock2");
+                synchronized (lock2){
                     System.out.println("Thread2: has lock1 and lock2");
                 }
-                System.out.println("Thread2 Released lock1");
+                System.out.println("Thread2 Released lock2");
             }
-            System.out.println("Thread 2 released lock2");
+            System.out.println("Thread 2 released lock1");
         }
     }
 
