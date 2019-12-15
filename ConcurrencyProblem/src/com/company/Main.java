292dba74102bb04b90441967350d69794c8a2ce7
KevinW <bird831205@hotmail.com>
2019-12-15 14:11:59 -0500

    2 threads on account

--- ConcurrencyProblem/src/com/company/Main.java
+++ ConcurrencyProblem/src/com/company/Main.java
@@ -7,6 +7,28 @@ public class Main {
 
         BankAccount account = new BankAccount("12345-678",1000.00);
 
+        Thread t1 =  new Thread() {
+            public void run() {
+                account.deposit(300.00);
+                System.out.println("t1 deposited 300");
+                account.withdraw(50.00);
+                System.out.println("t1 withdrawn 50");
+
+            }
+        };
+
+        Thread t2= new Thread(){
+            public void run() {
+                account.deposit(203.75);
+                System.out.println("t2 deposited 203.75");
+                account.withdraw(100);
+                System.out.println("t2 withdrawn 100");
+            }
+        };
+
+        t1.start();
+        t2.start();
+
 
     }
 }
