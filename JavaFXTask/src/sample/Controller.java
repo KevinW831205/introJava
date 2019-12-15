245e897387d92cb58ef694a8dfc3e3cca91afa1e
KevinW <bird831205@hotmail.com>
2019-12-15 08:45:36 -0500

    progress bar

--- JavaFXTask/src/sample/Controller.java
+++ JavaFXTask/src/sample/Controller.java
@@ -1,34 +1,45 @@
 package sample;
 
-import javafx.application.Platform;
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class Controller {
 
     private Task<ObservableList<String>> task;
 
+
     @FXML
     private ListView listView;
 
-    public void initialize(){
+    @FXML
+    private ProgressBar progressBar;
+
+    public void initialize() {
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
                 Thread.sleep(1000);
 
-                final ObservableList<String> employees = FXCollections.observableArrayList(
+                String[] names = {
                         "name1",
                         "name2",
                         "name3",
                         "name4",
                         "name5",
                         "name6"
-                );
+                };
+
+                ObservableList<String> employees = FXCollections.observableArrayList();
+
+                for(int i=0; i<6; i++){
+                    employees.add(names[i]);
+                    updateProgress(i+1,6);
+                    Thread.sleep(200);
+                }
                 return employees;
             }
         };
@@ -37,7 +48,7 @@ public class Controller {
     }
 
     @FXML
-    public void buttonPressed(){
+    public void buttonPressed() {
         new Thread(task).start();
     }
 }
