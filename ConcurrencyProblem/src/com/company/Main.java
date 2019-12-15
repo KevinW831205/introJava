f12d99cc63e74950940909f6d1ccd8d8810040b4
KevinW <bird831205@hotmail.com>
2019-12-15 14:07:45 -0500

    concurrency practice initialization

---
+++ ConcurrencyProblem/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        BankAccount account = new BankAccount("12345-678",1000.00);
+
+
+    }
+}
