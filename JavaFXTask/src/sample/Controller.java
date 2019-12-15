70ca85d3b37e962b05e0c2a7bed5c07b5da95569
KevinW <bird831205@hotmail.com>
2019-12-15 08:37:54 -0500

    attempt to property bind list view

--- JavaFXTask/src/sample/Controller.java
+++ JavaFXTask/src/sample/Controller.java
@@ -1,21 +1,27 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class Controller {
 
     private Task<ObservableList<String>> task;
+
+    @FXML
+    private ListView listView;
 
     public void initialize(){
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
                 Thread.sleep(1000);
-                return FXCollections.observableArrayList(
+
+                final ObservableList<String> employees = FXCollections.observableArrayList(
                         "name1",
                         "name2",
                         "name3",
@@ -23,13 +29,15 @@ public class Controller {
                         "name5",
                         "name6"
                 );
+                return employees;
             }
         };
+        listView.itemsProperty().bind(task.valueProperty());
+
     }
 
     @FXML
     public void buttonPressed(){
         new Thread(task).start();
-
     }
 }
