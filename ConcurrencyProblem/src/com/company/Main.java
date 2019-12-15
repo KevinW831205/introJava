1bc68162b7e728ae6c09e5dc76c1b095143c82f6
KevinW <bird831205@hotmail.com>
2019-12-15 14:14:56 -0500

    using 2 runnables

--- ConcurrencyProblem/src/com/company/Main.java
+++ ConcurrencyProblem/src/com/company/Main.java
@@ -3,32 +3,54 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-        BankAccount account = new BankAccount("12345-678",1000.00);
+        BankAccount account = new BankAccount("12345-678", 1000.00);
 
-        Thread t1 =  new Thread() {
+//        Thread t1 =  new Thread() {
+//            public void run() {
+//                account.deposit(300.00);
+//                System.out.println("t1 deposited 300");
+//                account.withdraw(50.00);
+//                System.out.println("t1 withdrawn 50");
+//
+//            }
+//        };
+//
+//        Thread t2= new Thread(){
+//            public void run() {
+//                account.deposit(203.75);
+//                System.out.println("t2 deposited 203.75");
+//                account.withdraw(100);
+//                System.out.println("t2 withdrawn 100");
+//       };
+
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
             public void run() {
                 account.deposit(300.00);
                 System.out.println("t1 deposited 300");
                 account.withdraw(50.00);
                 System.out.println("t1 withdrawn 50");
-
             }
-        };
+        });
 
-        Thread t2= new Thread(){
+        Thread t2 = new Thread(new Runnable() {
+            @Override
             public void run() {
                 account.deposit(203.75);
                 System.out.println("t2 deposited 203.75");
                 account.withdraw(100);
                 System.out.println("t2 withdrawn 100");
             }
-        };
+        });
+
 
         t1.start();
         t2.start();
-
-
     }
+
+
 }
+
