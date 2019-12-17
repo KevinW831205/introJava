cb688f2535f1c087611b439772d5f220fdc07d20
KevinW <bird831205@hotmail.com>
2019-12-16 20:55:54 -0500

    multiple line lambda expression

--- JavaLambdaExpression/src/com/company/Main.java
+++ JavaLambdaExpression/src/com/company/Main.java
@@ -3,25 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-        new Thread(new CodeToRun()).start();
+//        new Thread(new CodeToRun()).start();
+//
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from runnable2");
+//            }
+//        }).start();
+//
+//        new Thread(() -> System.out.println("Printing with Lambda Expression")).start();
 
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Printing from runnable2");
-            }
+        new Thread(()->{
+            System.out.println("print1");
+            System.out.println("print2");
+            int x =5;
+            x += 2;
+            System.out.println("x"+x);
         }).start();
-
-        new Thread( () -> System.out.println("Printing with Lambda Expression")).start();
 
     }
 }
 
 
-class CodeToRun implements Runnable{
+class CodeToRun implements Runnable {
     @Override
     public void run() {
         System.out.println("Printing from runnable");
