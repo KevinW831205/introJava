ba7427d36777cb6598badbd834e0b45691d2e4d0
KevinW <bird831205@hotmail.com>
2019-12-23 22:00:13 -0500

    biFunction

--- JavaLambdaExpression/src/com/company/Employees/Main.java
+++ JavaLambdaExpression/src/com/company/Employees/Main.java
@@ -3,10 +3,7 @@ package com.company.Employees;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Function;
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class Main {
 
@@ -117,6 +114,14 @@ public class Main {
         Function chainedFunction = upperCase.andThen(firstName);
 
         System.out.println("Chained Function "+chainedFunction.apply(employees.get(0)));
+
+
+        BiFunction<String, Employee, String> concatAge = (String name, Employee employee) ->{
+            return name.concat(" "+employee.getAge());
+        };
+
+        String upperName = upperCase.apply(employees.get(0));
+        System.out.println("BiFunction "+ concatAge.apply(upperName,employees.get(0)));
     }
 
     private static String getAName(Function<Employee, String> getName, Employee employee){
