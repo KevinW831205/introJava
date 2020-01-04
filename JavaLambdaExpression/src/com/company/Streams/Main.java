afd959923f65ea0f08a77d54c322e15b300c31df
KevinW <bird831205@hotmail.com>
2020-01-04 08:11:55 -0500

    more on streams peek and concat

--- JavaLambdaExpression/src/com/company/Streams/Main.java
+++ JavaLambdaExpression/src/com/company/Streams/Main.java
@@ -3,14 +3,15 @@ package com.company.Streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
         List<String> bingoNumbers = Arrays.asList(
-                "N40","N36",
-                "B6","B12",
-                "G15","G34","G23","g64",
+                "N40", "N36",
+                "B6", "B12",
+                "G15", "G34", "G23", "g64",
                 "O71",
                 "I26"
         );
@@ -30,8 +31,21 @@ public class Main {
         bingoNumbers
                 .stream()
                 .map(String::toUpperCase)
-                .filter(s->s.startsWith("G"))
+                .filter(s -> s.startsWith("G"))
                 .sorted()
                 .forEach(System.out::println);
+
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "O71");
+        Stream<String> inNumberStream = Stream.of("N40", "N36", "I26", "I29", "I17", "O71");
+        Stream<String> concatStream = Stream.concat(ioNumberStream, inNumberStream);
+//        System.out.println(concatStream.count());
+        System.out.println("----------------------");
+        System.out.println(concatStream
+                .distinct()
+                .peek(System.out::println)
+                .count());
+
+
     }
 }
