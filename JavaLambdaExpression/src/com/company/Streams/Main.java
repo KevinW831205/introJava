62e4d784eb8c41284cab5f817b2b384f4b5cf9b2
KevinW <bird831205@hotmail.com>
2020-01-04 16:36:08 -0500

    collect

--- JavaLambdaExpression/src/com/company/Streams/Main.java
+++ JavaLambdaExpression/src/com/company/Streams/Main.java
@@ -5,6 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -70,6 +71,17 @@ public class Main {
         departments.stream().flatMap(department -> department.getEmployees().stream())
                 .forEach(System.out::println);
 
+        List<String> sortedGNumbers = bingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println("--------------------");
+        for(String s : sortedGNumbers){
+            System.out.println(s);
+        }
 
     }
 }
