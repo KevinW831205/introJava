d4c049eaa82f9d03ccdb52f6e3ea7e5e15159477
KevinW <bird831205@hotmail.com>
2020-01-05 05:41:48 -0500

    more practice

--- JavaLambdaExpression/src/com/company/Practice/Problem2.java
+++ JavaLambdaExpression/src/com/company/Practice/Problem2.java
@@ -3,26 +3,39 @@ package com.company.Practice;
 import java.util.function.Function;
 
 public class Problem2 {
+//    public static String everySecondChar(String source) {
+//        StringBuilder returnVal = new StringBuilder();
+//        for (int i = 0; i < source.length(); i++) {
+//            if (i % 2 == 1) {
+//                returnVal.append(source.charAt(i));
+//            }
+//        }
+//        return returnVal.toString();
+//    }
 
-    public static String everySecondChar(String source) {
-        StringBuilder returnVal = new StringBuilder();
-        for (int i = 0; i < source.length(); i++) {
-            if (i % 2 == 1) {
-                returnVal.append(source.charAt(i));
+
+    public static void main(String[] args) {
+
+        Function<String, String> solution = (String source) -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
             }
-        }
-        return returnVal.toString();
+            return returnVal.toString();
+        };
+
+//        System.out.println(solution.apply("1234567890"));
+
+        String x = everySecondChar(solution, "1234");
+        System.out.println(x);
+
     }
 
-    Function<String, String> solution = (String source)->{
-        StringBuilder returnVal = new StringBuilder();
-        for (int i = 0; i < source.length(); i++) {
-            if (i % 2 == 1) {
-                returnVal.append(source.charAt(i));
-            }
-        }
-        return returnVal.toString();
-    };
+    public static String everySecondChar(Function<String,String> func, String s){
+        return func.apply(s);
+    }
 
 
 }
