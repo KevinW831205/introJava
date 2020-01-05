671d2f2308b216aff390834c91f07e9668c8fb29
KevinW <bird831205@hotmail.com>
2020-01-04 22:26:07 -0500

    more practice

---
+++ JavaLambdaExpression/src/com/company/Practice/Problem2.java
@@ -0,0 +1,28 @@
+package com.company.Practice;
+
+import java.util.function.Function;
+
+public class Problem2 {
+
+    public static String everySecondChar(String source) {
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    }
+
+    Function<String, String> solution = (String source)->{
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    };
+
+
+}
