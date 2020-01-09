88dc81ce513ecce61bd30c3867ab14abc6afbbfc
KevinW <bird831205@hotmail.com>
2020-01-09 03:15:49 -0500

    all with .

--- IntroRegularExpression/src/com/company/Main.java
+++ IntroRegularExpression/src/com/company/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String s = "I am a string.";
+        System.out.println(s);
+        String s2 = s.replaceAll("I","You");
+        System.out.println(s2);
 
+
+        String alphanumeric = "abDeeeF12Ghhiiijkl99z";
+        System.out.println(alphanumeric);
+        System.out.println(alphanumeric.replaceAll(".","Y"));
 
     }
 }
